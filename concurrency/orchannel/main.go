8a6f01102c58b9253377ce3a367d00542af5f409
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-07-16 14:46:24 +0545

    feat: add code for or-channel

---
+++ concurrency/orchannel/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Takes in multiple channels and outputs single channel
+// If any of the cahnnels in variadic argument is closed
+// returned channel should also close.
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+
+	return orDone
+}
+
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+	return c
+}
+
+func main() {
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v\n", time.Since(start))
+
+}
